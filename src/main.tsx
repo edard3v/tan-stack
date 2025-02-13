ae3669a5c2d19ff11868e7e26ac14c9e981d94d7
edard3v <edard3v@gmail.com>
2025-02-13 09:53:40 -0500

    instalando tanStack

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import "./main.css";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
